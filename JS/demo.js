747bb279f1c7f72b8aca5cff14d26e890cbf68af
THIERRY DE VILLE D'AVRAY <villedavray_charles@yahoo.fr>
2018-02-19 23:36:02 +0100

    Create demo.js

---
+++ JS/demo.js
@@ -0,0 +1,12 @@
+var demo = {
+    scene: "TEST_QUARTIER",
+    incremental: false,
+    binary: false,
+    doNotUseCDN: false,
+    collisions: true,
+    offline: true,
+    freeze: true,
+    onload: function () {
+
+    }
+};
